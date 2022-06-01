5472517a3ab39a62fadd4d94afae58dbe057591c
kuniseichi <kuniseichi@gmail.com>
2022-06-01 17:05:54 +0800

    all

--- es/es.sh
+++ es/es.sh
@@ -1,6 +1,6 @@
-docker run -d --name elasticsearch  --net somenetwork -p 9200:9200 \
+docker run -d --name elasticsearch  --net docker_test -p 9200:9200 \
  -p 9300:9300 -e "discovery.type=single-node" -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
   -e "ELASTIC_PASSWORD=password" \
- -v /home/cbc/project/opensource/es/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
+ -v /root/project/opensource/es/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
  elasticsearch:7.13.3
 
--- es/kibana.sh
+++ es/kibana.sh
@@ -1,1 +1,4 @@
- docker run -d --name kibana --net somenetwork -p 5601:5601 kibana:7.13.3
+docker run -d --name kibana --net docker_test \
+ -p 5601:5601 \
+ -v /root/project/opensource/es/kibana.yml:/usr/share/kibana/config/kibana.yml \
+ kibana:7.13.3
